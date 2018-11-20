1b3ceb932a451000eac38b0f8c4655367a069fe5
Sai Chandramouli <schand31@asu.edu>
2018-11-19 21:15:21 -0800

    webpacker with npm instead of yarn, cuz wynaut

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module RailsReactJestWebpack
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.webpacker.check_yarn_integrity = false
   end
 end
